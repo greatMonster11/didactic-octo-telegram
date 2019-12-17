d4f27311c53bca21d92a9740f9e0d6fe39d1b63f
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2019-12-17 23:51:11 +0700

    import snipcart

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,11 @@ interface IIndexProps {
 const Index = (props: IIndexProps) => {
   return (
     <div className="app">
+	    <Head>
+		  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+		  <script src="https://cdn.snipcart.com/scripts/2.0/snipcart.js" data-api-key={process.env.API_KEY} id="snipcart"></script> 
+			<link href="https://cdn.snipcart.com/themes/2.0/base/snipcart.min.css" rel="stylesheet" type="text/css" />
+		</Head>
       <Header />
       <main className="main">
         <ProductList products={props.products} />
