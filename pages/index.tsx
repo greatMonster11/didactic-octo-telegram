48b2b4f31e4c224419ac92a31ef2942830b9c0cd
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2019-12-17 23:55:23 +0700

    add - latest changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,8 @@ import { IProduct } from "../components/Product"
 import Footer from "../components/Footer"
 import Head from "next/head"
 
+import "../styles.scss"
+
 interface IIndexProps {
   products: IProduct[]
 }
@@ -23,12 +25,15 @@ const Index = (props: IIndexProps) => {
       <Footer />
     </div>
   )
-}
+};
 
 Index.getInitialProps = async ({ req }) => {
   return {
     products: [
       {id: "nextjs_halfmoon", name: "Halfmoon Betta", price: 25.00, image: "../static/halfmoon.jpg", description: "The Halfmoon betta is arguably one of the prettiest betta species. It is recognized by its large tail that can flare up to 180 degrees."} as IProduct,
+      {id: "nextjs_dragonscale", name: "Dragon Scale Betta", price: 35, image: "../static/dragonscale.jpg",description: "The dragon scale betta is a rarer and higher maintenance fish. It is named by its thick white scales covering his sides that looks like dragon scale armor."} as IProduct,
+      {id: "nextjs_crowntail", name: "Crowntail Betta", price: 7.50, image: "../static/crowntail.jpg", description: "The crowntail is pretty common, but interesting none the less. It's recognized by the shape of its tail that has an appearance of a comb."} as IProduct,
+      {id: "nextjs_veiltail", name: "Veiltail Betta", price: 5.00, image: "../static/veiltail.jpg", description: "By far the most common betta fish. You can recognize it by its long tail aiming downwards."} as IProduct,
     ]
   }
 }
